beece28f0ff47f0b8824ae4de1345a45f92f699a
FindOZ <lawyer712@gmail.com>
2024-07-30 20:46:07 +0200

    Homework lesson 3.2

---
+++ 03/homework3.2.py
@@ -0,0 +1,16 @@
+print( "LIST START" )
+
+print("Enter the list")
+number_1 = []
+number_1 = input().split()
+print(number_1)
+element = number_1.pop()
+number_1.insert(0,element)
+print(number_1)
+
+
+
+
+
+
+
