6d215859704b2a011ea6d0bd372add20c134af35
FindOZ <lawyer712@gmail.com>
2024-08-03 23:21:46 +0200

    Homework lesson 4.2

---
+++ 04/homework4.2.py
@@ -0,0 +1,40 @@
+a = [0, 1, 7, 2, 4, 8]
+c = []
+for b in a:
+    if a.index(b) == 0 or a.index(b) % 2 == 0:
+       c.append(b)
+print(c)
+d = (sum(c))
+print(d)
+e = d * a[-1]
+print(e)
+a = [1, 3, 5]
+c = []
+for b in a:
+    if a.index(b) == 0 or a.index(b) % 2 == 0:
+       c.append(b)
+print(c)
+d = (sum(c))
+print(d)
+e = d * a[-1]
+print(e)
+a = [6]
+c = []
+for b in a:
+    if a.index(b) == 0 or a.index(b) % 2 == 0:
+       c.append(b)
+print(c)
+d = (sum(c))
+print(d)
+e = d * a[-1]
+print(e)
+a = []
+c = []
+for b in a:
+    if a.index(b) == 0 or a.index(b) % 2 == 0:
+       c.append(b)
+print(c)
+d = (sum(c))
+print(d)
+e = d * a[-1]
+print(e)
