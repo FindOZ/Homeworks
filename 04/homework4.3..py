72c356e796f514f0a245c676f99b4a251a06bc1f
FindOZ <lawyer712@gmail.com>
2024-08-03 23:59:34 +0200

    Homework lesson 4.4

--- 04/homework4.3..py
+++ 04/homework4.3..py
@@ -1,7 +1,8 @@
 import random
 
 print( "LIST START" )
-b = [random.randint(3, 10) for a in range(10)]
+a = random.randint(1, 100)
+b = [random.randint(3, 10) for b in range(a)]
 print(b)
 c = b.pop(0)
 d = b.pop(1)
