0b812d5037974061023f811950bef471620976ce
FindOZ <lawyer712@gmail.com>
2024-08-18 13:13:28 +0200

    Homework6.1

--- 06/homework6.1.py
+++ 06/homework6.1.py
@@ -1,16 +1,14 @@
 
 import string
 
-a = input("Введите две буквы через дефис:")
+a = input("Введите две буквы через дефис: ")
 
 start_letter, end_letter = a.split('-')
 
 start_index = string.ascii_letters.index(start_letter)
 end_index = string.ascii_letters.index(end_letter)
-if start_index < end_index - 1:
-    missed_letters = string.ascii_letters[start_index + 1:end_index]
-else:
-    missed_letters = ""
+
+missed_letters = string.ascii_letters[start_index:end_index + 1]
 
 print(missed_letters)
 
