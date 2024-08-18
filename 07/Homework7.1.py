f535b753532df116dd85ce86054435cdadb88278
FindOZ <lawyer712@gmail.com>
2024-08-18 16:14:18 +0200

    Homework7.1

---
+++ 07/Homework7.1.py
@@ -0,0 +1,6 @@
+def say_hi(name, age):
+    name = str(input("Напишіть своє ім'я: "))
+    age = int(input("Напишіть свій вік: "))
+    return f"Hi. My name is {name} and I'm {age} years old"
+print(say_hi("", 0))
+print('ОК')
